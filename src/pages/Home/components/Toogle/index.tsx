ff410b0f14cc0cc4759dfddd20255fc05e3482df
luizokada <luizfernandookada@gmail.com>
2023-05-05 19:54:13 -0300

    moon

--- src/pages/Home/components/Toogle/index.tsx
+++ src/pages/Home/components/Toogle/index.tsx
@@ -1,16 +1,27 @@
-import { ToogleProps } from "../ToogleProps"
-import { Container } from "./styles"
+import { ToogleProps } from '../ToogleProps';
+import { Container } from './styles';
 
-const Toogle:React.FC<ToogleProps> = ({isDark,setIsDark}) => {
-    
 
-    return (
-        <Container isDark={ isDark}>
-<div onClick={()=>setIsDark(prev=>!prev)} className="toggle"/>
 
-        </Container>
-    
-    )
-}
+const Toogle: React.FC<ToogleProps> = ({ isDark, setIsDark }) => {
 
-export default Toogle
+  return (
+      <Container isDark={isDark}>
+          <div className="background-day"></div>
+          <div className="background-night"></div>
+          <div onClick={() => setIsDark((prev) => !prev)} className="toggle" >
+              
+              <div className="moon">
+              </div>
+                  <div className="crater1"></div>
+                  <div className="crater2"></div>
+                    <div className="crater3"></div>
+              <div className="sun"></div>
+
+
+      </div>
+    </Container>
+  );
+};
+
+export default Toogle;
--- src/pages/Home/components/Toogle/styles.ts
+++ src/pages/Home/components/Toogle/styles.ts
@@ -1,36 +1,111 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 type ContainerProps = {
-    isDark: boolean
-}
-export const Container = styled.div<ContainerProps>`
-    width: 200px;
-    height: 90px;
-    border-radius: 50px;
-     background: ${props => props.isDark ? `linear-gradient(180deg, #364BBA 0%, #A979D9 100%)` : 'linear-gradient(to bottom, #9be2fe 0%, #67d1fb 100%)'};
-     transition:  background 1s linear;
-    position: relative;
-    box-shadow: inset 0 0 20px black;;
-    .toggle {
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        ${props => props.isDark ? 'left: 10px' : 'left: calc(100% - 80px)'};
-        transition: left 1s linear, background-color 1s linear;
-        width: 70px;
-        height: 70px; 
-        background-color: ${props => props.isDark ?  `rgba(220, 240, 255, 1)` : 'rgb(252, 229, 112)'};
-        border-radius: 50%}
-    `;
+  isDark: boolean;
+};
 
-export const NightSky = styled.div`
-    width: 100%;
-    height: 100%;
-    >span{
+const ANIMATION_DURATION = 1;
+
+
+export const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 90px;
+  border-radius: 50px;
+  position: relative;
+  transition: left ${ANIMATION_DURATION}s linear;
+  box-shadow: inset 0 0 20px black;
+  overflow: hidden;
+  cursor: pointer;
+
+  .toggle {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    ${(props) => (props.isDark ? 'left: 10px' : 'left: calc(100% - 80px)')};
+    transition: left ${ANIMATION_DURATION}s linear, background-color ${ANIMATION_DURATION}s linear;
+    width: 70px;
+    height: 70px;
+    background-color: ${(props) =>
+      props.isDark ? `rgba(220, 240, 255, 1)` : 'rgb(252, 229, 112)'};
+    border-radius: 50%;
+    overflow: hidden;
+
+    ::before {
+        content: ' ';
         position: absolute;
-        background-color: white;
+        width: 70px;
+        height: 70px;
+        background-color:rgba(220, 240, 255, 1);
+        left: 100%;
+        top: 0;
+        ${({isDark}) => (isDark ? 'left:0%;' : 'left:100%;')}
+        transition: left ${ANIMATION_DURATION}s linear;
+
+  }
+  .moon {
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    top: 50%;
+    left: 50%;
+    background: linear-gradient(180deg, #D6E9FF 0%, #CDE4FF 100%);;
+    ${({ isDark }) => (isDark ? 'left:0%;' : 'left:500%;')}
+     transition: left ${ANIMATION_DURATION}s linear;
 
     }
+    .crater1 {
+       position: absolute;
+       width: 10px;
+       height: 10px;
+       border-radius: 50%;
+       top: 10%;
+
+       background: linear-gradient(180deg, #D6E9FF 0%, #CDE4FF 100%);;
+       ${({ isDark }) => (isDark ? 'left:30%;' : 'left:100%;')}
+       transition: left ${ANIMATION_DURATION}s linear;
+
+    }
+
+    .crater2 {
+       position: absolute;
+       width: 30px;
+       height: 30px;
+       border-radius: 50%;
+       top: 10%;
+       background: linear-gradient(180deg, #D6E9FF 0%, #CDE4FF 100%);;
+       ${({ isDark }) => (isDark ? 'left:80%;' : 'left:100%;')}
+       transition: left ${ANIMATION_DURATION}s linear;
+    }
+
+}
+
+  .background-day {
+      background:linear-gradient(to bottom, #9be2fe 0%, #67d1fb 100%);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    ${({isDark}) => (isDark ? 'left:-100%;' : 'left:0;')}
+    transition: left ${ANIMATION_DURATION}s linear;
+  }
+
+  .background-night {
+    background: linear-gradient(180deg, #364BBA 0%, #A979D9 100%);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    ${({ isDark }) => (!isDark ? 'left:100%;' : 'left:0;')}
+    transition: left ${ANIMATION_DURATION}s linear;
+  }
 `;
-    
+
+export const NightSky = styled.div`
+  width: 100%;
+  height: 100%;
+  > span {
+    position: absolute;
+    background-color: white;
+  }
+`;
